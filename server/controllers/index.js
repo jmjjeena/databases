1a1767746189c6a580575bb08a51c194487ad0cc
jmjjeena <jmj.jeena@gmail.com>
2019-03-14 19:42:13 -0700

    Connected web server to database

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,45 @@
 var models = require('../models');
+// var mysql = require('../mysql');
+// var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      // console.log("Got a GET request for the messages");
+      // res.status(404).send('Sorry, we cannot find that!');
+      res.send('Got get request');
+      models.messages.get((err) => {
+        if (err) {
+          res.status(404).send('Not found');
+        } else {
+          res.status(200).send('Found')
+          res.end();
+        }
+      });
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      console.log(req.body);
+      res.send('post route')
+      models.messages.post((err) => {
+        if (err) {
+          res.status(404).send('Not found');
+        } else {
+          res.status(200).send('Found')
+          res.end();
+        }
+      });
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      models.users.get(res)
+    },
+    post: function (req, res) {
+      console.log(req.body);
+      // models.users.post(req.body, res)
+      res.end();
+    }
   }
 };
-
--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,31 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    // a function which produces all the messages
+    get: function (callback) {
+      var queryStr = 'SELECT messages.id, messages.messages, rooms.room_name, users.user_name';
+      db.query(queryStr, function (err, results) {
+        if (err) {
+          throw err;
+        }
+        callback(null, results);
+      });
+    },
+    post: function (messages, rooms_id, users_id, callback) {
+      var queryStr = 'INSERT INTO messages (messages, users.id, rooms.id) VALUES ("' + messages + ' , ' + users_id + ' , ' + rooms_id + ' )';
+      db.query(queryStr, function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log('successful');
+      })
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () { },
+    post: function () { }
   }
 };
 
